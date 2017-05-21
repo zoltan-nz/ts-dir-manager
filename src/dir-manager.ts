bf87e86af960a87f1dc1e5411abb00a57796b64f
Zoltan <zdebre@gmail.com>
2017-05-21 22:07:00 +1200

    First test pass

--- src/dir-manager.ts
+++ src/dir-manager.ts
@@ -1,4 +1,3 @@
-import * as fs from 'async-file';
 import * as path from 'path';
 import * as process from 'process';
 import Debug = require('debug');
@@ -23,101 +22,31 @@ export interface IFile {
     children: IFile[];
 }
 
-class File implements IFile {
+export interface ITree {
+    [ index: string ]: IFile | ITree;
+}
 
-    public children: File[] = [];
-    public name: string;
-    public ext: string;
-    public content: string;
-    public type: FileType;
-    private ready: Promise<boolean>;
+export class TreeMaker {
 
-    private stat: fs.Stats;
-
-    constructor(private absoluteFilePath: string) {
-        this.ready = new Promise((resolve, reject) => {
-            this.setupStat().then(() => {
-                    this.name = path.basename(this.absoluteFilePath);
-                    this.ext = path.extname(this.absoluteFilePath);
-                    this.type = this.getType();
-
-                    if (this.stat.isDirectory()) {
-                        this.buildChildren().then(() => {
-                            process.stdout.write('yey');
-                            resolve(true);
-                        });
-                    }
-                }
-            );
-        });
-
-    }
-
-    public async getTree(): Promise<{ [index: string]: File[] }> {
-        return new Promise((resolve: { [index: string]: File[] }, reject) => {
-            this.ready.then(result => {
-                console.log('isReadyAfterThen', result);
-
-                const tree: { [index: string]: File[] } = {};
-                tree[this.name] = this.children;
-                return resolve(tree);
-            });
-        });
-    }
-
-    private async setupStat() {
-        await fs.stat(this.absoluteFilePath).then(stat => this.stat = stat);
-    }
-
-    private getType() {
-        if (this.stat.isDirectory()) return FileType.Directory;
-        if (this.stat.isSymbolicLink()) return FileType.SymLink;
-        return FileType.File;
-    }
-
-    private async buildChildren() {
-        const children: string[] = await fs.readdir(this.absoluteFilePath);
-
-        await children.forEach(async child => {
-            const childAbsoluteFilePath: string = path.resolve(this.absoluteFilePath, child);
-            this.children.push(new File(childAbsoluteFilePath));
-        });
-
-        await Promise.all(this.children.map(file => file.ready));
+    public getTree(): ITree {
+        return {
+            fixtures: {
+                'dir-1': {}
+            }
+        };
     }
 }
 
-(async () => {
-    const
-        mainDirectory = process.argv[2];
-    const
-        pwd = process.cwd();
-
-    const
-        resolvedDirectory = path.resolve(pwd, mainDirectory);
-
-    debug(
-        'resolvedDirectory'
-        ,
-        resolvedDirectory
-    );
-
-    const
-        dir = new File(resolvedDirectory);
-    const
-        tree = await
-            dir
-                .getTree();
-
-    console
-        .log(JSON
-
-                .stringify(tree)
-
-            ,
-            null
-            ,
-            2
-        ); // tslint:disable-line no-console
-})
-();
+// (async () => {
+//     const mainDirectory = process.argv[2];
+//     const pwd = process.cwd();
+//
+//     const resolvedDirectory = path.resolve(pwd, mainDirectory);
+//
+//     debug('resolvedDirectory', resolvedDirectory);
+//
+//     const treeMaker = new TreeMaker(resolvedDirectory);
+//     const tree = await treeMaker.getTree();
+//
+//     console.log(JSON.stringify(tree), null, 2); // tslint:disable-line no-console
+// })();
--- test/unit/dir-manager-test.ts
+++ test/unit/dir-manager-test.ts
@@ -2,25 +2,33 @@ import {expect} from 'chai';
 
 import * as path from 'path';
 
+import { TreeMaker } from '../../src/dir-manager';
+
 const FIXTURE_DIRECTORY = '../fixtures/fixture-directory';
 
-describe('DirManager', () => {
+describe('TreeMaker', () => {
 
-    it('#lsDir - should return directory tree as an Object', () => {
-        const directory = path.resolve(process.cwd(), FIXTURE_DIRECTORY);
+    it ('new - should create an instance', () => {
+        const treeMaker = new TreeMaker();
 
-        const expectedObject = {
-            'fixture-directory': {
-                'dir-1': {
-                    'dir-1-dir-2': {
-                        'file-2.txt': true
-                    },
-                    'file-3.txt': true
-                },
-                'file-1.txt': true
-            }
-        };
-
-        expect(1).is.equal(1);
+        expect(treeMaker).is.instanceof(TreeMaker);
     });
+
+    // it('#lsDir - should return directory tree as an Object', () => {
+    //     const directory = path.resolve(process.cwd(), FIXTURE_DIRECTORY);
+    //
+    //     const expectedObject = {
+    //         'fixture-directory': {
+    //             'dir-1': {
+    //                 'dir-1-dir-2': {
+    //                     'file-2.txt': true
+    //                 },
+    //                 'file-3.txt': true
+    //             },
+    //             'file-1.txt': true
+    //         }
+    //     };
+    //
+    //     expect(1).is.equal(1);
+    // });
 });
