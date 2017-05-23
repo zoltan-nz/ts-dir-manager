aa4b44eab8ecd0aa0e7c8d0f5c1a266e29a08db7
Zoltan <zdebre@gmail.com>
2017-05-23 22:07:00 +1200

    Add tree builder test and implementation

--- src/tree-maker.ts
+++ src/tree-maker.ts
@@ -11,43 +11,47 @@ export enum FileType {
 
 export interface IFile {
     path: string;
-    name?: string;
+    name: string;
     ext?: string;
     type?: FileType;
     content?: string;
-    children?: IFile[];
+    children: TreeMaker[];
     stat: fs.Stats;
 }
 
 export interface ITree {
-    [ index: string ]: IFile | ITree;
+    [ index: string ]: ITree[];
 }
 
 export class TreeMaker {
 
-    private static getFile(path: string): IFile {
+    private static getFile(pathName: string): IFile {
         let stat: fs.Stats;
 
         try {
-            stat = fs.statSync(path);
+            stat = fs.statSync(pathName);
         } catch (e) {
             console.error(e.message); // tslint:disable-line no-console
             throw (e);
         }
 
+        const name = path.basename(pathName);
+
         return {
-            path,
+            children: [],
+            name,
+            path: pathName,
             stat
         };
     }
 
-    private static getChildren(parentFile: IFile): IFile[] {
-        if (!parentFile.stat.isDirectory()) { return []; }
+    private static getChildren(parentFile: IFile): TreeMaker[] {
+        if (!parentFile.stat.isDirectory()) return [];
 
         let childrenFileNames: string[] = fs.readdirSync(parentFile.path);
         childrenFileNames = childrenFileNames.map(name => path.resolve(parentFile.path, name));
 
-        return childrenFileNames.map(name => new TreeMaker(name).entry);
+        return childrenFileNames.map(name => new TreeMaker(name));
     }
 
     public entry: IFile;
@@ -55,17 +59,18 @@ export class TreeMaker {
     constructor(public entryPath: string) {
         assert.ok(!_.isEmpty(entryPath), 'TreeMaker expect a non empty string.');
 
-        const entry = TreeMaker.getFile(entryPath);
+        const entry: IFile = TreeMaker.getFile(entryPath);
         entry.children = TreeMaker.getChildren(entry);
-
         this.entry = entry;
     }
 
     public getTree(): ITree {
-        return {
-            fixtures: {
-                'dir-1': {}
-            }
-        };
+        const tree: ITree = {};
+        const name: string = this.entry.name;
+
+        const children: TreeMaker[] = this.entry.children;
+
+        tree[name] = _.isEmpty(children) ? [] : children.map(child => child.getTree());
+        return tree;
     }
 }
--- test/unit/tree-maker-test.ts
+++ test/unit/tree-maker-test.ts
@@ -2,45 +2,61 @@ import {expect} from 'chai';
 
 import * as path from 'path';
 
-import { TreeMaker } from '../../src/tree-maker';
+import {TreeMaker} from '../../src/tree-maker';
 
 const FIXTURE_DIRECTORY = path.resolve(__dirname, '../fixtures/fixture-directory');
 const FIXTURE_FILE = path.resolve(__dirname, '../fixtures/fixture-directory/file-1.txt');
 
 describe('TreeMaker', () => {
 
-    it ('new - should create an instance', () => {
+    it('new - should create an instance', () => {
         const treeMaker = new TreeMaker(FIXTURE_DIRECTORY);
 
         expect(treeMaker, 'should create an instance').is.instanceof(TreeMaker);
     });
 
-    it ('#getFile - should raise an errors', () => {
+    it('#getFile - should errors when the file does not exists', () => {
         expect(() => new TreeMaker('not-existing.file'), 'not existing file should throw an ENOENT error').to.throw(/ENOENT/);
         expect(() => new TreeMaker(''), 'empty string as param should throw an assert error').to.throw();
     });
 
-    it ('entry - should contain the tree object', () => {
+    it('entry - should contain the tree object', () => {
         const treeMaker = new TreeMaker(FIXTURE_DIRECTORY);
 
         expect(treeMaker.entry).is.not.empty;
     });
 
-    it ('entry.children - should contain the children objects', () => {
+    it('entry.children - should contain the children objects', () => {
         const treeMaker = new TreeMaker(FIXTURE_DIRECTORY);
-        const dir1Tree  = new TreeMaker(`${FIXTURE_DIRECTORY}/dir-1`);
+        const dir1Tree = new TreeMaker(`${FIXTURE_DIRECTORY}/dir-1`);
         const file1Tree = new TreeMaker(`${FIXTURE_DIRECTORY}/file-1.txt`);
 
         expect(treeMaker.entry.children).length(2);
-        expect(treeMaker.entry.children).deep.equal([dir1Tree.entry, file1Tree.entry]);
+        expect(treeMaker.entry.children).deep.equal([dir1Tree, file1Tree]);
     });
 
-    it ('#getTree - should return an Object', () => {
+    it('#getTree - should return an Object', () => {
         const treeMaker = new TreeMaker(FIXTURE_DIRECTORY);
         const expected = {
-            fixtures: {
-                'dir-1': {}
-            }
+            'fixture-directory': [
+                {
+                    'dir-1': [
+                        {
+                            'dir-1-dir-2': [
+                                {
+                                    'file-2.txt': []
+                                }
+                            ]
+                        },
+                        {
+                            'file-3.txt': []
+                        }
+                    ]
+                },
+                {
+                    'file-1.txt': []
+                }
+            ]
         };
 
         expect(treeMaker.getTree()).deep.equal(expected);
